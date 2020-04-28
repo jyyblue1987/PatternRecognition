f52fb93be60fd304ee4b17989b1a743761578d89
jyyblue <jyyblue1987@outlook.com>
2020-04-28 18:44:02 +0300

    [add] add pca, cross_validation.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.decomposition import PCA
+from sklearn.model_selection import cross_val_score
 
 # Load the Pandas libraries with alias 'pd'
 import pandas as pd
@@ -19,8 +20,9 @@ pca_dim = 2
 pca = PCA(n_components=pca_dim)
 pca.fit(X_Train)
 
-X_Train = pca.transform(X_Train)
 print(pca.explained_variance_ratio_)
+
+# X_Train = pca.transform(X_Train)
 
 df = pd.read_csv(r'D_Test1.csv')
 
@@ -30,26 +32,30 @@ X_Test = test_data[1:, 1:]
 Y_Test = test_data[1:, [0]]
 Y_Test = Y_Test.ravel()
 
-X_Test = pca.transform(X_Test)
+# X_Test = pca.transform(X_Test)
 
 # ================= Navie Bayes ===============================
 gnb = GaussianNB()
-naive_model = gnb.fit(X_Train, Y_Train) # train
-navie_y = naive_model.predict(X_Test)
 
-# Evaluate Error Count
-getErrorCount(navie_y, Y_Test)
+scores = cross_val_score(gnb, X_Train, Y_Train, cv=5)
+print("Navid Bayes Cross Validation Scores = ", scores)
+print("Navid Bayes Cross Validation Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+naive_model = gnb.fit(X_Train, Y_Train) # train
+svm_score = gnb.score(X_Test, Y_Test)
+print("Navie Bayes Final Test Score = ", svm_score)
 
 # ================= SVM ===============================
 # Train
 clf = svm.SVC(decision_function_shape='ovr')
-clf.fit(X_Train, Y_Train)
+scores = cross_val_score(clf, X_Train, Y_Train, cv=5)
+print("SVM Cross Validation Scores = ", scores)
+print("SVM Cross Validation Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
 # Predict
-svm_y = clf.predict(X_Test)
-
-# Evaluate Error Count
-getErrorCount(svm_y, Y_Test)
+clf.fit(X_Train, Y_Train)
+svm_score = clf.score(X_Test, Y_Test)
+print("SVM Final Test Score = ", svm_score)
 
 
 
