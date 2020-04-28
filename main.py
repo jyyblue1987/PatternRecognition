17f7e4be82f419ae2a3a626e2216f6893dcd0087
jyyblue <jyyblue1987@outlook.com>
2020-04-28 19:16:06 +0300

    [add] add preprocessing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
+from sklearn import preprocessing
 from sklearn.decomposition import PCA
 from sklearn.model_selection import cross_val_score
 
@@ -16,6 +17,8 @@ X_Train = train_data[1:, 1:]
 Y_Train = train_data[1:, [0]]
 Y_Train = Y_Train.ravel()
 
+scaler = preprocessing.StandardScaler().fit(X_Train)
+# X_Train = scaler.transform(X_Train)
 pca_dim = 2
 pca = PCA(n_components=pca_dim)
 pca.fit(X_Train)
@@ -33,6 +36,7 @@ Y_Test = test_data[1:, [0]]
 Y_Test = Y_Test.ravel()
 
 # X_Test = pca.transform(X_Test)
+# X_Test = scaler.transform(X_Test)
 
 # ================= Navie Bayes ===============================
 gnb = GaussianNB()
